ddb71a8a22cefae26a3f79b72ba7f4a5972148c9
anichu <anis.cse5.bu@gmail.com>
2022-10-26 13:27:13 +0600

    added coureses get api

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const courses = require("./data/courses.json");
+const categories = require("./data/categories.json");
 const app = express();
 
 app.use(cors());
+
+app.get("/courses", (req, res) => {
+	console.log(courses.length);
+	res.send(courses);
+});
+app.get("/categories", (req, res) => {
+	console.log(categories.length);
+	res.send(categories);
+});
+
 const port = process.env.PORT || 5000;
 
-console.log(port);
+app.listen(port, () => {
+	console.log("Learn web running on port", port);
+});

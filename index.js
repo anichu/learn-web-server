4fe3291afcda265fa6cf2f1f0c02ef607cbc0243
anichu <anis.cse5.bu@gmail.com>
2022-10-26 13:46:47 +0600

    added fetch data api by category

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get("/courses", (req, res) => {
 	console.log(courses.length);
 	res.send(courses);
 });
+app.get("/courses/:categoryName", (req, res) => {
+	const categoryName = req.params.categoryName;
+	const filteredCourses = courses.filter(
+		(course) => course.category === categoryName
+	);
+	res.send(filteredCourses);
+});
 app.get("/categories", (req, res) => {
 	console.log(categories.length);
 	res.send(categories);
